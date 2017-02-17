a332dfdcbd6378ab9eba950257c4e7e280d5c2bf
Andrey Altukhov <andrey.altukhov@gmail.com>
2017-02-17 13:51:48 +0200

    Add a back-end project that is based on Spring Boot

---
+++ back-end/src/main/java/com/intapp/BackEndApplication.java
@@ -0,0 +1,12 @@
+package com.intapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndApplication.class, args);
+	}
+}
